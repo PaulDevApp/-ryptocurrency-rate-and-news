b0e2a615204fb07e5f77b27828e753e02184282c
PaulDevApp <paul.dev.app@gmail.com>
2021-08-11 00:41:13 +0300

    commit 2 diffUtil

---
+++ src/main/java/com/appsforlife/cryptocourse/utils/ItemDiffUtil.kt
@@ -0,0 +1,22 @@
+package com.appsforlife.cryptocourse.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.appsforlife.cryptocourse.pojo.CoinPriceInfo
+
+class ItemDiffUtil(
+    private val oldList: List<CoinPriceInfo>,
+    private val newList: List<CoinPriceInfo>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition].fromSymbol == newList[newItemPosition].fromSymbol
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[oldItemPosition]
+    }
+}
