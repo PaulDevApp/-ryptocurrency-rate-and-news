f628c82b72c1b46a25f679ff03d98d86005dda74
PaulDevApp <paul.dev.app@gmail.com>
2021-08-30 01:50:41 +0300

    commit 8 add Splashscreen API, final version

--- src/main/java/com/appsforlife/cryptocourse/activities/MainActivity.kt
+++ src/main/java/com/appsforlife/cryptocourse/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.appsforlife.cryptocourse.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.appsforlife.cryptocourse.R
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+        
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
